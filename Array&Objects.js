95b33be26692a5b45cbe1a381c49806ea536b83f
Mollah Tareq <tareqcseuiu@gmail.com>
2021-10-15 23:51:34 +0600

    arr obj prb 1 with bonus

--- Array&Objects.js
+++ Array&Objects.js
@@ -1,9 +1,10 @@
-function sumAll(start,end){
+function sumAll(start,end,step){
     let sum = 0;
-    for(let i=start; i<=end; i++){
-        sum+= i;
+    for(let i=start; i<=end; i+=step){
+        sum += i;
+        console.log(i);
         
     }
     return sum;
 }
-console.log(sumAll(1,10));
+console.log(sumAll(1,10,2));
